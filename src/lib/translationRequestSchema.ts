a0572f95aa363aaf002edc581fd157bdb9319ce1
Quozul <30729291+Quozul@users.noreply.github.com>
2026-01-25 11:29:20 +0100

    fix: trim translation request

--- src/lib/translationRequestSchema.ts
+++ src/lib/translationRequestSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { supportedLanguageSchema } from '$lib/Language';
 
 export const translationRequestSchema = z.object({
-	text: z.string(),
+	text: z.string().trim(),
 	source_language: supportedLanguageSchema,
 	target_language: supportedLanguageSchema
 });
